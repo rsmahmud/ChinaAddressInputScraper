27a512c9480bc23e71503eb69ce2fcb783d3856e
Rasel Mahmud <rrss.mahmud@gmail.com>
2022-09-15 20:08:20 +0600

    data[province][city] = [districts] filtered district by district[parent] == city[name]

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def get_district_list(adcode):
     except Exception as e:
         print(f"Error getting district list:\nError: {e}\nData: {res.content}")
         district_list = []
-    else:
-        district_list = [r['name'] for r in district_list]
+
     print(f"status code :{res.status_code}\tfound districts: {len(district_list)} with adcode={adcode}")
     return district_list
 
@@ -29,6 +28,7 @@ def get_city_list(adcode):
     except Exception as e:
         print(f"Error getting city list:\nError: {e}\nData: {res.content}")
         city_list = []
+
     print(f"status code :{res.status_code}\tfound cities: {len(city_list)} with adcode={adcode}")
     return city_list
 
@@ -39,18 +39,18 @@ def get_data_set():
     # province_list = [r['name'] for r in province_list]
     print(f"status code :{res.status_code}\tfound provinces: {len(province_list)}")
     for i, province in enumerate(province_list):
-        print(f"# {i+1}. getting cities for province: {province['name']} {province['adcode']}")
+        print(f"\n# {i+1}. getting cities for province: {province['name']} {province['adcode']}")
         DATA_SET[province['name']] = {}
         city_list = get_city_list(province['adcode'])
-
-        DATA_SET[province['name']]['cities'] = [r['name'] for r in city_list]
-        DATA_SET[province['name']]['districts'] = get_district_list(city_list[0]['adcode']) if city_list else []
-
-
-        # for j, city in enumerate(city_list):
-        #     print(f"## {i+1}.{j+1}. getting district for city: {city['name']} {city['adcode']}")
-        #     DATA_SET[province['name']][city['name']] = get_district_list(city['adcode'])
-        #
+        # DATA_SET[province['name']]['cities'] = [r['name'] for r in city_list]
+        # DATA_SET[province['name']]['districts'] = district_list
+        district_list = get_district_list(city_list[0]['adcode']) if city_list else []
+        for j, city in enumerate(city_list):
+            print(f"\n## {i + 1}.{j + 1}. getting district for city: {city['name']} {city['adcode']}")
+            # district_list = get_district_list(city['adcode'])
+            districts = [r['name'] for r in district_list if r['parent'] == city['name']]
+            DATA_SET[province['name']][city['name']] = districts
+            print(f"## {i+1}.{j+1}. filtered districts: {len(districts)}")
 
     # json.dump(list(DATA_SET.keys()), open('china_address_input_provinces.json', 'w', encoding='utf8'), indent=4, ensure_ascii=False)
     # json.dump(DATA_SET, open('china_address_input_provinces_with_cities.json', 'w', encoding='utf8'), indent=4, ensure_ascii=False)
